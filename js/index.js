dea2ba56c6df3da92cdeace703b914e7a8ea2e93
Yobil101 <biltusaha49@gmail.com>
2021-02-28 00:19:40 +0530

    index done

--- js/index.js
+++ js/index.js
@@ -1,21 +1,40 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml3');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-// animation for the top most words 
-anime.timeline({loop: true})
+var textWrapper = document.querySelector(".ml3");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+// animation for the top most words
+anime
+  .timeline({ loop: true })
   .add({
-    targets: '.ml3 .letter',
-    opacity: [0,1],
+    targets: ".ml3 .letter",
+    opacity: [0, 1],
     easing: "easeInOutQuad",
     duration: 2250,
-    delay: (el, i) => 150 * (i+1)
-  }).add({
-    targets: '.ml3',
+    delay: (el, i) => 150 * (i + 1),
+  })
+  .add({
+    targets: ".ml3",
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
-    delay: 1000
+    delay: 1000,
   });
 
 // image slideshow
- 
+var indexValue = 0;
+function slideShow() {
+  setTimeout(slideShow, 2500);
+  var x;
+  const img = document.querySelectorAll("img");
+  for (x = 0; x < img.length; x++) {
+    img[x].style.display = "none";
+  }
+  indexValue++;
+  if (indexValue > img.length) {
+    indexValue = 1;
+  }
+  img[indexValue - 1].style.display = "block";
+}
+slideShow();
